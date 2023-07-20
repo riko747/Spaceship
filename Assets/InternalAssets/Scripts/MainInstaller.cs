fa156605451f9a8762e99847ea2b7c1f64cf6ae5
Vitaliy Bosiy <riko199610241601@gmail.com>
2023-07-20 11:56:27 +0300

    Switched to Android

---
+++ Assets/InternalAssets/Scripts/MainInstaller.cs
@@ -0,0 +1,11 @@
+using Zenject;
+
+namespace InternalAssets.Scripts
+{
+    public class MainInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+        }
+    }
+}
